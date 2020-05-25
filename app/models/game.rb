056809bbacf00c81736364d16e0fb7be8edadcd3
Gerard Altamirano <galtamiranor@outlook.es>
2020-05-25 18:27:28 -0500

    Polymorphic association

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,10 @@
 class Game < ApplicationRecord
+  has_many :reviews, as: :reviewable
+
   has_many :expansions, class_name: "Game", foreign_key: "parent_id"
   belongs_to :parent, class_name: "Game", optional: true
 
   has_many :companies, through: :involved_companies
   has_and_belongs_to_many :platforms, :genres
+
 end
