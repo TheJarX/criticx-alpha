93c69fa5b43582a414be9590158feae786231618
Gerard Altamirano <galtamiranor@outlook.es>
2020-05-28 00:33:01 -0500

    authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :games, only: [:index, :show, :new, :create, :edit, :update,  :destroy]
   resources :companies, only: [:index, :show, :new, :create, :edit, :update,  :destroy]
+  resources :users, only: [:new, :create]
+  # Routes for sessions
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  delete 'session', to: 'sessions#destroy'
 end
